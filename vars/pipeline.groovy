322f886f204c1e2a39b8f11ef40564a480d5685a
Sandeep <chaugule.sandeep@gmail.com>
2020-08-01 12:37:26 +0530

    Added pipeline

---
+++ vars/pipeline.groovy
@@ -0,0 +1,19 @@
+def call(body){
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+   pipeline {
+       agent any
+       stages {
+       stage('Echo from shared lib'){
+
+           steps {
+               echo "${config.message}"
+           }
+       }
+       }
+   }
+
+}
