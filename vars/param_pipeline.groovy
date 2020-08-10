4808c3ed8511feab5a3a0feeb6206d2c242c94d9
Sandeep <chaugule.sandeep@gmail.com>
2020-08-10 23:03:08 +0530

    add new shared library

---
+++ vars/param_pipeline.groovy
@@ -0,0 +1,30 @@
+def call(Map config){
+    pipeline{
+        agent any
+        stages{
+            stage('Windows'){
+                when {
+                    expression{
+                        config.OS = 'true'
+                    }
+                    
+                }
+                steps{
+                    echo 'Running windows'
+                }
+            }
+             stage('Linux'){
+                when {
+                    expression{
+                        config.OS = 'false'
+                    }
+                    
+                }
+                steps{
+                    echo 'Running windows'
+                }
+            }           
+
+        }
+    }
+}
